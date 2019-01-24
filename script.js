e224ca5c2690f6a2d88489f20c57af142d52a391
Vitalii <vet.polyanychko@gmail.com>
2019-01-24 22:58:08 +0200

    code formatting

--- script.js
+++ script.js
@@ -1,28 +1,28 @@
-"use strict"
+    "use strict"
 
 	class snackBar {
-		handleClick()
-		{
+	  handleClick() {
 		let snack = document.querySelector('.snackbar');
 		snack.classList.add('show');
 		snack.innerHTML="Default Text";
 		setTimeout( () => snack.classList.remove('show'),3000);
-		}
+	  }
 
-		handleClick1()
-		{
+	  handleClick1() {
 		let snack = document.querySelector('.snackbar');
 		let param = document.querySelector('#inpt').value;
-		if (param ==='')
-		{
-			snack.innerHTML='Input cannot be empty';
+
+		if (param ==='') {
+		  snack.innerHTML='Input cannot be empty';
 		}
-		else{ 
-		snack.innerHTML=param;
+
+		else { 
+		  snack.innerHTML=param;
 		}	
+		
 		snack.classList.add('show');
 		setTimeout( () => snack.classList.remove('show'),3000);
-		}
-}
-			
+	  }
+    }
+
 	let user = new snackBar();
