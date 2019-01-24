3f997e8b48693c353e62a14690d6084476d8b7d1
Vitalii <vet.polyanychko@gmail.com>
2019-01-24 18:43:05 +0200

    release v1

---
+++ script.js
@@ -0,0 +1,28 @@
+"use strict"
+
+	class snackBar {
+		handleClick()
+		{
+		let snack = document.querySelector('.snackbar');
+		snack.classList.add('show');
+		snack.innerHTML="Default Text";
+		setTimeout( () => snack.classList.remove('show'),3000);
+		}
+
+		handleClick1()
+		{
+		let snack = document.querySelector('.snackbar');
+		let param = document.querySelector('#inpt').value;
+		if (param ==='')
+		{
+			snack.innerHTML='Input cannot be empty';
+		}
+		else{ 
+		snack.innerHTML=param;
+		}	
+		snack.classList.add('show');
+		setTimeout( () => snack.classList.remove('show'),3000);
+		}
+}
+			
+	let user = new snackBar();
